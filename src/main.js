a79e47497b164bb3ab68bfb228f75611b1904a0f
renaldy23 <renaldyramadhan567@gmail.com>
2023-01-30 16:46:36 +0700

    fix dockerfile

--- src/main.js
+++ src/main.js
@@ -1,56 +1,56 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamList from './components/teams/TeamsList.vue'
-import UserList from './components/users/UsersList.vue'
-import TeamMember from './components/teams/TeamMembers.vue'
+import TeamList from './components/teams/TeamsList.vue';
+import UserList from './components/users/UsersList.vue';
+import TeamMember from './components/teams/TeamMembers.vue';
 
 const routes = createRouter({
-    history: createWebHistory(),
-    routes: [
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/teams',
+    },
+    {
+      name: 'teams',
+      path: '/teams',
+      components: { default: TeamList, footer: UserList },
+      children: [
         {
-            path: '/',
-            redirect: '/teams'
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMember,
+          props: true,
         },
-        {
-            name: 'teams',
-            path: '/teams',
-            components: { default: TeamList, footer: UserList },
-            children: [
-                {
-                    name: 'team-members',
-                    path: ':teamId',
-                    component: TeamMember,
-                    props: true
-                }
-            ]
-        },
-        {
-            path: '/users',
-            component: UserList
-        },
-        {
-            path: '/:notFound(.*)',
-            redirect: '/'
-        }
-    ],
-    linkActiveClass: 'active',
-    linkExactActiveClass: 'exact-active',
-    scrollBehavior(_, _2, savedPosition) {
-        if (savedPosition) {
-            return savedPosition
-        }
-        return { left: 0, top: 0 }
+      ],
+    },
+    {
+      path: '/users',
+      component: UserList,
+    },
+    {
+      path: '/:notFound(.*)',
+      redirect: '/',
+    },
+  ],
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'exact-active',
+  scrollBehavior(_, _2, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
     }
-})
-const app = createApp(App)
+    return { left: 0, top: 0 };
+  },
+});
+const app = createApp(App);
 routes.beforeEach(function (to, _, next) {
-    // if (to.name == "team-members") {
-    next()
-    // } else {
-    //     next({ name: 'team-members', params: { teamId: 't2' } })
-    // }
-})
-app.use(routes)
+  // if (to.name == "team-members") {
+  next();
+  // } else {
+  //     next({ name: 'team-members', params: { teamId: 't2' } })
+  // }
+});
+app.use(routes);
 app.mount('#app');
